6af2a38dfe9be17eea2d63900752d41e6a3792c8
Groza Sergiu <serioja90@gmail.com>
2015-11-20 19:14:08 +0100

    Implemented #permitted? function for Aclize::Acl::ControllerRegistry

--- lib/aclize/acl/controllers_registry.rb
+++ lib/aclize/acl/controllers_registry.rb
@@ -1,3 +1,9 @@
+# The policy adopted by Aclize is:
+#   1. By default all the controllers and paths are denied
+#   2. On rules conflict, the more restrictive rule will be used
+#   3. When permit and deny rules have the same restriction,
+#      deny rule will be used
+
 
 module Aclize
   class Acl::ControllersRegistry
@@ -5,33 +11,77 @@ module Aclize
     attr_reader :permitted, :denied
 
     def initialize
-      @permitted = {}
-      @denied    = {}
+      @permitted = {"*" => []}.nested_under_indifferent_access
+      @denied    = {"*" => []}.nested_under_indifferent_access
     end
 
     # add a new permit rule to controllers registry
-    def permit(name, only: nil, except: nil)
-      @permitted[name.to_s] ||= []
-      @denied[name.to_s]    ||= []
+    def permit(controller, only: nil, except: nil)
+      @permitted[controller] ||= []
+      @denied[controller]    ||= []
 
       if except
-        @permitted[name.to_s] = ["*"]
-        @denied[name.to_s]    = normalize(except)
+        @permitted[controller] = ["*"]
+        @denied[controller]    = normalize(except)
       elsif only
-        @permitted[name.to_s] = normalize(only)
+        @denied[controller]    = []
+        @permitted[controller] = normalize(only)
       else
-        @permitted[name.to_s] = "*"
+        @permitted[controller] = ["*"]
+        @denied[controller]    = []
       end
     end
 
-    # add a new deny rule to controllers registry
-    def deny(name, only: nil, except: nil)
-      # deny is the oposite of permit, so it
-      # is sufficient to invert :only with :except
-      permit name, only: only, except: except
+    # check if each action in the list is allowed for the specified controller
+    def permitted?(controller, *args)
+      @permitted[controller] ||= []
+      @denied[controller]    ||= []
+      actions = normalize(args)
+
+      if actions.empty?
+        return controller_permitted?(controller)
+      elsif controller_permitted?(controller)
+        # we know the there's at least one permitted action for this controller,
+        # so return false if there's at least one denied action in the list of actions to check
+        return false unless (actions & @denied[controller]).empty?
+
+        # we know that the actions aren't denied at controller level, so we could
+        # return true if all the actions are also permitted at controller level
+        return true if @permitted[controller].include?("*") || (actions & @permitted[controller]) == actions
+
+        # the actions aren't permitted at controller level, so if any of them is
+        # denied at global level, we will return false
+        return false unless (actions & @denied["*"]).empty?
+
+        # the actions aren't denied at global level, so we have to check if them
+        # are allowed at global level and return true if so
+        return true if @permitted["*"].include?("*") || (actions & @permitted["*"]) == actions
+      end
+
+      return false
     end
 
     protected
+
+    # check if the controller is permitted (at least one action in the controller should be permitted)
+    def controller_permitted?(controller)
+      # the simplies case is when there's a permission for at least one action for this controller
+      if @permitted[controller].empty?
+        # check if there're wildcard permissions
+        if @permitted["*"].empty?
+          # there isn't any global permission for the user
+          return false
+        else
+          # we have global permissions (for all the controllers), so we
+          # have to check if those actions weren't denied
+          denied_actions = @denied[controller] + @denied["*"]
+          return !(@permitted["*"] - denied_actions).empty?
+        end
+      else
+        # we are sure there's at least one permission for this controller
+        return true
+      end
+    end
 
     def normalize(items)
       result = items.nil? ? [] : items.is_a?(Array) ? items : [items]
