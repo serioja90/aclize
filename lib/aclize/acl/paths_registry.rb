075da63c6fbd188355d9e3771c4be4d8445405d9
Groza Sergiu <serioja90@gmail.com>
2015-11-20 15:03:15 +0100

    Implemented Aclize::Acl::PathsRegistry

--- lib/aclize/acl/paths_registry.rb
+++ lib/aclize/acl/paths_registry.rb
@@ -2,5 +2,27 @@
 module Aclize
   class Acl::PathsRegistry
 
+    attr_reader :permitted, :denied
+
+    def initialize
+      @permitted = []
+      @denied    = []
+    end
+
+    def permit(*paths)
+      @permitted += normalize(paths)
+      @permitted.uniq!
+    end
+
+    def deny(*paths)
+      @denied += normalize(paths)
+      @denied.uniq!
+    end
+
+    protected
+
+    def normalize(items)
+      return items.nil? ? [] : items.is_a?(Array) ? items : [items]
+    end
   end
 end
